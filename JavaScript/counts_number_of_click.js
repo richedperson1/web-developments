97bf14a518374886498e26058e4f08df0c2345f8
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-11-05 15:24:38 +0530

    Modifing [counts_number_of_click.js] which take input in addEventListener also.

--- JavaScript/counts_number_of_click.js
+++ JavaScript/counts_number_of_click.js
@@ -1,11 +1,44 @@
+// var num = 0
+
+// let box_ele = document.getElementById("select_box")
+
+// param = document.getElementById("counters")
+
+// box_ele.onclick = () => {
+
+//     num++;
+//     param.innerHTML = num
+// }
+
 var num = 0
-
-let box_ele = document.getElementById("select_box")
-
-param = document.getElementById("text_for_box")
-
-box_ele.onclick = () => {
-
+const check_click = () => {
+    param = document.getElementById("counters")
     num++;
-    param.innerHTML = param.textContent.slice(0, 10) + " " + num
+    param.innerHTML = num;
+
 }
+
+param = document.getElementById("select_box")
+
+
+parent.addEventListener("click", check_click)
+
+/*
+
+we can also take input in addEventListener 
+
+by following 
+
+
+
+var num = 0
+const check_click = (a,b) => {
+    param = document.getElementById("counters")
+    num = a+b;
+    param.innerHTML = num;
+
+}
+parent.addEventListener("click", check_click(5,6));
+
+
+*/
