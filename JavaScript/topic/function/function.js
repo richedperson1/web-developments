98b0555c9d4e21282232a01d692cebc4e17cce48
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-03-17 09:24:24 +0530

    adding function

---
+++ JavaScript/topic/function/function.js
@@ -0,0 +1,8 @@
+function actual_value() {
+    console.log("First Element")
+    console.log("Second Element")
+    console.log("Third Element")
+}
+
+let first = actual_value();
+console.log(first);
--- JavaScript/topic/loop/index.js
+++ JavaScript/topic/loop/index.js
@@ -1,9 +1,29 @@
 let arr = [1, 2, 3, "rutvik", "Jaiswal"]
 
+// Loop in the javascript
+// for (let name in arr) {
+//     console.log("Hello words", name)
+// }
+// for (let fname of arr) {
+//     console.log("Hello -->", fname)
+// }
 
-for (let name in arr) {
-    console.log("Hello words", name)
-}
-for (let fname of arr) {
-    console.log("Hello -->", fname)
-}
+
+// JavaScript array methods
+
+let arr_sub = arr.slice(1, 5)
+
+// console.log(arr_sub)
+
+
+// Deleting Element from the array
+
+arr.splice(0, 3, "Bhirud")
+
+// console.log("Final array : ", arr)
+
+// Adding new element in perticular index
+arr.splice(2, 0, "Patil")
+
+
+console.log(arr)
