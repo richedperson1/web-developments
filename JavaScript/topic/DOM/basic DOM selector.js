d9f83aef30ce19822cbbf75e355bf8777d6284dc
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-03-19 18:52:41 +0530

    adding toggle btn in each div using js

--- JavaScript/topic/DOM/basic DOM selector.js
+++ JavaScript/topic/DOM/basic DOM selector.js
@@ -1,6 +1,37 @@
+// Method 1 to add orange class in the elements
+
 let first_box = document.getElementById("box-num-1")
 
-let myfun = () => {
+let myfun = (first_box) => {
     first_box.classList.toggle("orange");
 }
-first_box.addEventListener("click", myfun)
+
+let addingToggleBtn = () => {
+
+    let boxes = document.getElementsByClassName("boxes")
+    let num = boxes.length
+    for (let ind = 0; ind < num; ind++) {
+        let child = boxes[ind]
+        child.addEventListener("click", function() {
+            myfun(child);
+        })
+
+    }
+}
+
+addingToggleBtn();
+
+// Method 2
+// const myfun2 = (box) => {
+//     box.classList.toggle("orange");
+//   }
+
+//   let boxes = document.querySelectorAll(".boxes");
+
+//   let addingToggleBtn2 = () => {
+//     boxes.forEach(box => {
+//         box.addEventListener("click", function() {
+//             myfun2(box);
+//         });
+//     });
+//   }
