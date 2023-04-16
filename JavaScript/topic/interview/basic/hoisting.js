54e89cc5b01208647adc612ec74b0851e2dd51b7
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-16 21:23:49 +0530

    adding hoisting in the JS

---
+++ JavaScript/topic/interview/basic/hoisting.js
@@ -0,0 +1,14 @@
+/*
+
+In the JS, Before executing the code 
+Excution contect is created which will have two section 
+
+1. Memory assign to the varible and function itselt
+
+This memory is for global scope
+Following thing are going to stored in the memory while it started excuting
+    1. All varible which declare using "Var" keyword
+    2. All function which declare using "function" keyword
+    3. Array function which declare using "var" keyword
+
+*/
