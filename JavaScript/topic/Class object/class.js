a3ca80ad34a318f9a658db9c991b1d135d6bc2b9
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-17 19:33:37 +0530

    Adding class method in js

--- JavaScript/topic/Class object/class.js
+++ JavaScript/topic/Class object/class.js
@@ -1,10 +1,22 @@
-class emp {
-    constructor(name) {
+
+
+
+class Emp {
+    constructor(name, lastName = "Jaiswal", Job = "USA") {
         this.first_name = name;
-        console.log("My name is", this.first_name);
+        this.lastName = lastName;
+        this.Job = Job;
+        // console.log("My name is", this.first_name);
+        // console.log("Last name is", this.lastName);
+        // console.log("And Job Location is", this.Job);
         return 0;
+    }
+    emp_actual() {
+        this.element = "Hello worlds";
     }
 }
 
-rutvik = new emp("Rutvik")
+let rutvik = new Emp("Rutvik")
+console.log(rutvik);
+rutvik.emp_actual()
 console.log(rutvik);
---
+++ JavaScript/topic/function/function_declaration.js
@@ -0,0 +1,8 @@
+
+console.log(firstDeclare);
+
+
+var firstDeclare = function () {
+    console.log("Hello word");
+    return "Hello";
+}
