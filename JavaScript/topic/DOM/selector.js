87fe4fabd24396b922b1d66b620e6b59317c960e
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-24 12:41:46 +0530

    adding selector in js

---
+++ JavaScript/topic/DOM/selector.js
@@ -0,0 +1,25 @@
+
+let first = document.getElementsByTagName("h1")
+let firstWord = "Rutvik The brand";
+let secondWord = "Rutvik Jaiswal the brand";
+
+var toggleSwitch = [0];
+// setInterval(() => {
+//     toggle = toggleSwitch[0]
+//     if (toggle) {
+//         first[0].innerHTML = firstWord;
+//         toggleSwitch[0] = 0
+//         // console.log(toggleSwitch);
+//     }
+//     else {
+//         first[0].innerHTML = secondWord;
+//         toggleSwitch[0] = 1
+
+//     }
+
+// }, 1000)
+
+
+let queryAll = document.querySelector("li")
+console.log(queryAll);
+queryAll.id
