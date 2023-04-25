b14b1a728c07fd19a44355c737bd0e203c6df26f
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-25 11:43:25 +0530

    Adding selector in the page

--- JavaScript/topic/DOM/selector.js
+++ JavaScript/topic/DOM/selector.js
@@ -4,20 +4,20 @@ let firstWord = "Rutvik The brand";
 let secondWord = "Rutvik Jaiswal the brand";
 
 var toggleSwitch = [0];
-// setInterval(() => {
-//     toggle = toggleSwitch[0]
-//     if (toggle) {
-//         first[0].innerHTML = firstWord;
-//         toggleSwitch[0] = 0
-//         // console.log(toggleSwitch);
-//     }
-//     else {
-//         first[0].innerHTML = secondWord;
-//         toggleSwitch[0] = 1
+setInterval(() => {
+    toggle = toggleSwitch[0]
+    if (toggle) {
+        first[0].innerHTML = firstWord;
+        toggleSwitch[0] = 0
+        // console.log(toggleSwitch);
+    }
+    else {
+        first[0].innerHTML = secondWord;
+        toggleSwitch[0] = 1
 
-//     }
+    }
 
-// }, 1000)
+}, 1000)
 
 
 let queryAll = document.querySelector("li")
