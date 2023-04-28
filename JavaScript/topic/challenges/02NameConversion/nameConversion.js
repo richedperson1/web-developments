cba9c90d7b82f29b75b1a96c03a60fda7369d649
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-28 20:01:51 +0530

    adding comment on nameConversion.js

--- JavaScript/topic/challenges/02NameConversion/nameConversion.js
+++ JavaScript/topic/challenges/02NameConversion/nameConversion.js
@@ -1,6 +1,16 @@
 
-let inputField = document.getElementById("text")
 
+/* 
+
+These lines of code are adding event listeners to the `inputField` HTML element. Whenever the user
+inputs text into the `inputField`, each of the corresponding functions (`camelCase`, `PacelCase`,
+`snake_case`, `Screaming_Snake_Case`, `kabab_case`, and `Screaming_kabab_Case`) will be called and
+will convert the input text into different formats and display the results in corresponding HTML
+elements.
+
+*/
+
+let inputField = document.getElementById("text")
 inputField.addEventListener("input", camelCase)
 inputField.addEventListener("input", PacelCase)
 inputField.addEventListener("input", snake_case)
@@ -78,6 +88,10 @@ function kabab_case() {
 
 }
 
+/**
+ * The function converts a string input in camel case to screaming snake case and displays the result
+ * in an HTML element.
+ */
 function Screaming_Snake_Case() {
 
     let camelCaseInput = document.getElementById("screaming-snake-case")
@@ -97,6 +111,10 @@ function Screaming_Snake_Case() {
 
 }
 
+/**
+ * The function converts a string input into screaming kebab case format and displays it in an HTML
+ * element.
+ */
 function Screaming_kabab_Case() {
 
     let camelCaseInput = document.getElementById("screaming-kebab-case")
@@ -118,6 +136,9 @@ function Screaming_kabab_Case() {
 
 
 
+/* This code is selecting all the `p` elements inside the HTML elements with class `case_div` and
+setting their `display` property to `none`. This is likely done to hide the output elements before
+the user inputs any text to be converted. */
 const btnActive = document.querySelectorAll(".case_div p");
 
 btnActive.forEach((val) => {
@@ -126,6 +147,15 @@ btnActive.forEach((val) => {
 
 
 
+/* 
+
+This code is selecting the HTML element with the ID "convert-btn" and adding an event listener to
+it. When the button is clicked, the code checks the current display state of the first `p` element
+inside the HTML elements with class `case_div`. If the display state is "none", meaning the output
+elements are hidden, the code changes the display state of all `p` elements inside the `case_div` to
+"inline-block", making them visible. 
+
+*/
 const btnConvert = document.getElementById("convert-btn")
 
 btnConvert.addEventListener("click", () => {
